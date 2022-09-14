9cc5421ec044e3a7782517bae0013f275df7d3a0
Bogdan <shakirovbogdan0@gmail.com>
2022-09-14 22:59:41 +0300

    Initial Commit

---
+++ TrafficLight1Lissen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight1Lissen
+//
+//  Created by 123 on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
